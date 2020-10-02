28d4e48e5e6714cf0dd48a7ee96975ef17600ad5
unknown <s19B702@shohoku.ac.jp>
2020-10-02 11:40:01 +0900

    ごめんなさい、成約？までできてない

---
+++ app/src/main/java/jp/ac/shohokus19b702/graphicssample/CustomView2.java
@@ -0,0 +1,21 @@
+package jp.ac.shohokus19b702.graphicssample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView2 extends View {
+    public CustomView2(Context context) {
+        super(context);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
